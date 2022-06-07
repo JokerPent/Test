6e5d674e9ef37903a18fd54d33fb78a77b18f2af
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-06-07 22:31:25 +0100

    fix: removing the bug on the insertion of process on the clas

--- main.c
+++ main.c
@@ -1,26 +1,35 @@
-#include<stdio.h>
-#include<locale.h>
+#include <stdio.h>
+#include <locale.h>
 #include "execution.c"
 #include "interface.h"
 
-int main(){
-    setlocale(LC_ALL,"Portuguese");
-    
-    TSimpleList *class1 = (TSimpleList*) malloc(sizeof(TSimpleList));
-    if(class1 == NULL) return NO_SPACE;
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
 
-    TSimpleList *class2 = (TSimpleList*) malloc(sizeof(TSimpleList));
-    if(class2 == NULL) return NO_SPACE;
-    
-    TSimpleList *printer = (TSimpleList*) malloc(sizeof(TSimpleList));
-    if(printer == NULL) return NO_SPACE;
-    
-    TCircleList *cpu = (TCircleList*) malloc(sizeof(TCircleList));
-    if(cpu == NULL) return NO_SPACE;
+    TSimpleList *class1 = (TSimpleList *)malloc(sizeof(TSimpleList));
+
+    if (class1 == NULL)
+        return NO_SPACE;
+
+    TSimpleList *class2 = (TSimpleList *)malloc(sizeof(TSimpleList));
+
+    if (class2 == NULL)
+        return NO_SPACE;
+
+    TSimpleList *printer = (TSimpleList *)malloc(sizeof(TSimpleList));
+
+    if (printer == NULL)
+        return NO_SPACE;
+
+    TCircleList *cpu = (TCircleList *)malloc(sizeof(TCircleList));
+
+    if (cpu == NULL)
+        return NO_SPACE;
 
     /**TJob *jobs = (TJob*) malloc(sizeof(TJob));
     if(jobs == NULL) return NO_SPACE;*/
-   
+
     int code = -2;
     init_simple_list(class1, 1);
     init_simple_list(class2, 2);
@@ -29,9 +38,9 @@ int main(){
 
     int num_simulation;
     int cod = -2;
-    //TProcess *output = NULL;
+    // TProcess *output = NULL;
 
-    //O tempo de simulação digitado pelo usuário
+    // O tempo de simulação digitado pelo usuário
     num_simulation = write_num();
 
     /*
@@ -49,7 +58,7 @@ int main(){
                         output = remove_in_list_circle(cpu);
                         cod = insert_on_end_of_simple_list(printer, output);
                     }
-                    
+
                     else if(output->job->temp_process!=0){
                         cod = -2;
                         output = NULL;
@@ -79,21 +88,26 @@ int main(){
         }
     }*/
 
-    //Rascunho
-    for(int i = 1; i <= num_simulation; i++){
-        if(i%12==0){
-            TProcess *process = (TProcess*) malloc(sizeof(TProcess));
-            if(process == NULL) return NO_SPACE;
-            init_job(process);
-            cod = insert_job_in_class(class1, class2, process);
-            if(process->job->id_class == 1)
-                //rintf("1\n");
-                show_structure(class1);
-            else if(process->job->id_class == 2)
-                //printf("2\n");
-                show_structure(class2);
+    // Rascunho
+    for (int i = 1; i <= num_simulation; i++)
+    {
+        if (i % 12 == 0)
+        {
+            TProcess *process = (TProcess *)malloc(sizeof(TProcess));
+            if (process == NULL)
+                return NO_SPACE;
 
+            init_job(process);
+
+            cod = insert_job_in_class(class1, class2, process);
+
+            if (process->job->id_class == 1)
+                // rintf("1\n");
+                show_structure(class1);
+            else if (process->job->id_class == 2)
+                // printf("2\n");
+                show_structure(class2);
         }
     }
-   return 0;
+    return 0;
 }
